d2ac4553f30472c17cdc9ef56131d432074dcc87
Ngoc Son Nguyen <nguyenngocson4391@gmail.com>
2019-09-03 22:13:16 +0700

    localization

---
+++ transaltions/index.js
@@ -0,0 +1,10 @@
+import * as Localization from "expo-localization";
+import i18n from "i18n-js";
+import vi from "./languages/vi.json";
+import en from "./languages/en.json";
+
+i18n.fallbacks = true;
+i18n.translations = { vi, en };
+i18n.locale = Localization.locale;
+
+export default i18n;
